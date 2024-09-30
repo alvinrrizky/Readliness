a6df107828e3b182039b9bf05be2b6aec55b1abb
alvin.rrizky <alvin.rrizky@gmail.com>
2024-10-01 06:55:33 +0700

    Add Project

---
+++ Project/Backend/readliness/src/main/java/com/gap/readliness/ReadlinessApplication.java
@@ -0,0 +1,13 @@
+package com.gap.readliness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadlinessApplication.class, args);
+	}
+
+}
---
+++ Project/Backend/readliness/src/test/java/com/gap/readliness/ReadlinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gap.readliness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadlinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
